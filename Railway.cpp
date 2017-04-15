d63183067e4abc61a1e9e0ece493252a83c2041e
DESKTOP-SCVHRLK\Denis <andreev.93@gmail.com>
2017-04-16 02:02:29 +0300

    commented

--- Railway.cpp
+++ Railway.cpp
@@ -8,12 +8,12 @@ Railway::~Railway()
 {
 }
 
-int Railway::getDistance(int from, int to) const
-{
-	return 0;
-}
-
-std::vector<std::vector<unsigned>> Railway::getGraph() const
-{
-	return stationsGraph;
-}
+//int Railway::getDistance(int from, int to) const
+//{
+//	return 0;
+//}
+//
+//std::vector<std::vector<unsigned>> Railway::getGraph() const
+//{
+//	return stationsGraph;
+//}
