7bf21d0232325fa8978611bba22a867e8b82995c
DESKTOP-SCVHRLK\Denis <andreev.93@gmail.com>
2017-04-15 23:19:31 +0300

    getStationsGraph comments

--- main.cpp
+++ main.cpp
@@ -9,14 +9,18 @@
 #include "Train.h"
 
 
-void getTrainsSchedules(std::vector<Train>& trains, const char* filename);
+void getTrainsSchedules(std::vector<Train>& trains, const char* filename);//функция считывания из файла информации о поездах
+void getStationsGraph(Railway& railway, const char* filename);//функция считывания из файла конфигурации ж/д сети
 
 int main() {
 
 	setlocale(LC_ALL, "RUS");//поддержка русской кодировки
 
-	std::vector<Train> trains;
-	getTrainsSchedules(trains, "schedule.txt");
+	std::vector<Train> trains;//вектор поездов
+	getTrainsSchedules(trains, "schedule.txt");//считываем из файла информации о поездах
+
+	Railway RZD;//ж/д сеть
+	getStationsGraph(RZD, "graph.txt");//считываем из файла конфигурацию ж / д сети
 
 	system("pause");
 	return 0;
@@ -24,34 +28,67 @@ int main() {
 
 void getTrainsSchedules(std::vector<Train>& trains, const char * filename)
 {
-	std::string line;
-	std::ifstream input(filename, std::ios_base::in);//входной файловый поток
+	std::string line;//буфер-строка
+	std::ifstream file(filename, std::ios_base::in);//входной файловый поток
 
-	if (!input.is_open()) {
+	if (!file.is_open()) { //проверка наличия файла
 		std::cout << "Файл не найден!" << std::endl;
 		system("pause");
 		//return -1;
 	}
 	else
 	{
-		std::getline(input, line);
-		while (std::getline(input, line)) {
-			Train train;
+		std::getline(file, line);//вывод строки в буфер
+		while (std::getline(file, line)) {//пока выводим строки в буфер
+			Train train;//объект класса Поезд
 
-			std::replace(line.begin(), line.end(), '-', ' ');
-			std::istringstream iss(line);
+			std::replace(line.begin(), line.end(), '-', ' ');//заменяем "-" на пробелы во входных данных о маршрутах
+			std::istringstream iss(line);//направляем данные из буфера-строки в строковый поток
 
-			iss >> train.name;
+			iss >> train.name;//первый элемент строки - название поезда
 
-			unsigned point;
-			while (iss >> point) {
-				train.path.push_back(point);
+			unsigned station;//станция в маршруте поезда
+			while (iss >> station) {
+				train.path.push_back(station);//заполняем вектор маршрута поезда
 			}
-			trains.push_back(train);
 
+			trains.push_back(train);//добавляем поезд в вектор поездов
 		}
 
-		input.close();
+		file.close();//закрываем файл
+	}
+
+
+}
+
+void getStationsGraph(Railway & railway, const char * filename)
+{
+	std::string line;//буфер-строка
+	std::ifstream file(filename, std::ios_base::in);//входной файловый поток
+
+	if (!file.is_open()) { //проверка наличия файла
+		std::cout << "Файл не найден!" << std::endl;
+		system("pause");
+		//return -1;
+	}
+	else
+	{
+		std::getline(file, line);//вывод строки в буфер
+		while (std::getline(file, line)) {//пока выводим строки в буфер
+			std::vector<unsigned> graphLine;//строка матрицы смежности графа
+			std::istringstream iss(line);//направляем данные из буфера-строки в строковый поток
+
+			iss.ignore(1024, ' ');//пропускаем первую строку
+
+			unsigned distance;//расстояние между станциями
+			while (iss >> distance) {
+				graphLine.push_back(distance);//заполняем строку матрицы смежности графа данными о расстояниях между станциями
+			}
+
+			railway.stationsGraph.push_back(graphLine);//добавляем строку к матрице смежности
+		}
+
+		file.close();//закрываем файл
 	}
 
 
