cc6af53338ddc1f76443fab9682f25a57675474c
DESKTOP-SCVHRLK\Denis <andreev.93@gmail.com>
2017-04-16 02:00:05 +0300

    getGraph

--- Railway.cpp
+++ Railway.cpp
@@ -12,3 +12,8 @@ int Railway::getDistance(int from, int to) const
 {
 	return 0;
 }
+
+std::vector<std::vector<unsigned>> Railway::getGraph() const
+{
+	return stationsGraph;
+}
--- Railway.h
+++ Railway.h
@@ -4,15 +4,14 @@
 #define _RAILWAY_H_
 
 class Railway {
-private:
-	
-
 public:
 	std::vector<std::vector<unsigned>> stationsGraph;
+
 	Railway();
 	~Railway();
 
-	int getDistance(int from,int to) const;
+	//int getDistance(int from,int to) const;
+	//std::vector<std::vector<unsigned>> getGraph() const;
 };
 
 #endif //_RAILWAY_H_
